ed2491c2588a34cf25ebafe5f3931c433bdbd647
whiplade <nmawira13@gmail.com>
2023-09-28 08:42:41 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -22,13 +22,49 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
+    
+    articles = Article.query.all()
 
-    pass
+    articles_list = []
+    for article in articles:
+        articles_list.append({
+            'id': article.id,
+            'author': article.author,
+            'title': article.title,
+            'content': article.content,
+            'preview': article.preview,
+            'minutes_to_read': article.minutes_to_read,
+            'date': article.date
+        })
+
+    return jsonify({'articles': articles_list})
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    
+    article = Article.query.get(id)
 
-    pass
+    if article:
+        
+        session['page_views'] = session.get('page_views', 0) + 1
+
+        if session['page_views'] > 3:
+            return make_response(jsonify({'message': 'Maximum pageview limit reached'}), 401)
+
+        
+        article_dict = {
+            'id': article.id,
+            'author': article.author,
+            'title': article.title,
+            'content': article.content,
+            'preview': article.preview,
+            'minutes_to_read': article.minutes_to_read,
+            'date': article.date
+        }
+
+        return jsonify(article_dict)
+    else:
+        return make_response(jsonify({'message': 'Article not found'}), 404)
 
 if __name__ == '__main__':
     app.run(port=5555)
